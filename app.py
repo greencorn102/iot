df3fad8db2ffec730e4341b94f0a50260b98ea7c
greencorn102 <126756036+greencorn102@users.noreply.github.com>
2026-01-22 16:53:20 -0500

    Update app.py refresh

--- app.py
+++ app.py
@@ -20,6 +20,40 @@ def receive_temperature(data: TempData):
 
 @app.get("/", response_class=HTMLResponse)
 def dashboard():
+    return """
+    <h1>Temperature Dashboard</h1>
+    <h2 id="temp">Loading...</h2>
+    <h3 id="msg"></h3>
+
+    <script>
+    async function fetchTemp() {
+        const res = await fetch('/latest');
+        const data = await res.json();
+
+        if (data.temperature === null) {
+            document.getElementById("temp").innerText = "No data yet";
+            return;
+        }
+
+        document.getElementById("temp").innerText =
+            "Current Temperature: " + data.temperature + " °C";
+
+        let msg = "Temperature normal";
+
+        if (data.temperature < 10) msg = "⚠️ Temperature too LOW!";
+        else if (data.temperature > 35) msg = "🔥 Temperature too HIGH!";
+
+        document.getElementById("msg").innerText = msg;
+    }
+
+    setInterval(fetchTemp, 3000); // every 3 seconds
+    fetchTemp();
+    </script>
+    """
+
+"""
+@app.get("/", response_class=HTMLResponse)
+def dashboard():
     if latest_temp is None:
         return "<h2>No data yet</h2>"
 
@@ -35,3 +69,4 @@ def dashboard():
     <h2>Current Temperature: {latest_temp} °C</h2>
     <h3>{message}</h3>
     """
+"""
