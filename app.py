392d7cd5c53fe566ee7b611b2092153d81506423
greencorn102 <126756036+greencorn102@users.noreply.github.com>
2026-02-04 14:26:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,43 @@ def receive_motion(data: MotionData):
 def get_latest():
     return {"motion": latest_motion}
 
-
 @app.get("/", response_class=HTMLResponse)
 def dashboard():
     return """
+    <h1>Motion Dashboard</h1>
+    <h2 id="motion">Sensing...</h2>
+    <h3 id="msg"></h3>
+
+    <script>
+    async function fetchTemp() {
+        const res = await fetch('/latest');
+        const data = await res.json();
+
+        if (data.motion === null) {
+            document.getElementById("motion").innerText = "* *";
+            document.getElementById("msg").innerText = "";
+            return;
+        }
+
+        document.getElementById("motion").innerText = data.motion;
+
+        let msg;
+        if (data.motion == 1)
+            msg = "MOTION DETECTED !!";
+        else
+            msg = "NO MOTION";
+
+        document.getElementById("msg").innerText = msg;
+    }
+
+    setInterval(fetchTemp, 3000);
+    fetchTemp();
+    </script>
+    """
+"""
+@app.get("/", response_class=HTMLResponse)
+def dashboard():
+    return 
     <h1>Motion Dashboard</h1>
     <h2 id="motion">Sensing...</h2>
     <h3 id="msg"></h3>
@@ -51,6 +84,8 @@ def dashboard():
     fetchMotion();
     </script>
     """
+    
+
 
 
 
